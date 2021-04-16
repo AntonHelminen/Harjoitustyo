21bace13a9c64a5ba72a35f4e6b3378e01d636af
Anton <80627322+Fawles@users.noreply.github.com>
2021-04-16 15:04:08 +0300

    updated DataFile_manager.java

--- app/src/main/java/com/example/harjoitustyo/DataFile_manager.java
+++ app/src/main/java/com/example/harjoitustyo/DataFile_manager.java
@@ -1,5 +1,6 @@
 package com.example.harjoitustyo;
 
+import android.content.Context;
 import android.util.Log;
 
 import java.io.BufferedReader;
@@ -8,37 +9,41 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+import javax.crypto.Cipher;
 
 public class DataFile_manager {
+    private static DataFile_manager df_manager = new DataFile_manager();
 
-    public DataFile_manager() {
-
+    public static DataFile_manager getInstance() {
+        return df_manager;
     }
-    public void writeFile() {
+    //When given context, writes all people's data to a txt file.
+    public void writeFile(Context context) {
         try {
-
+            OutputStreamWriter osw = new OutputStreamWriter(context.openFileOutput("Data_file.txt", Context.MODE_PRIVATE));
+            String s = "";
+            osw.write(s);
         }
+
         catch (IOException e) {
             Log.e("IOException", "Error in input");
         }
-        catch (FileNotFoundException e) {
-            Log.e("FileNotFoundException", "Could not find file");
-        }
     }
-    public void readFile() {
+    //When given context, reads all people's data from a txt file.
+    public void readFile(Context context) {
         try {
-            InputStream in = null;
+            InputStream in = context.openFileInput("Data_file.txt");
             BufferedReader br = new BufferedReader(new InputStreamReader(in));
             String s = "";
             while ((s=br.readLine())!=null) {
                 //readline
             }
+            in.close();
         }
         catch (IOException e) {
             Log.e("IOException", "Error in input");
-        }
-        catch (FileNotFoundException e) {
-            Log.e("FileNotFoundException", "Could not find file");
         }
     }
 }
