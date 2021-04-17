f0839ea78af92416f8f8e920326dcff29f59e189
Joona <80720342+Jobe333@users.noreply.github.com>
2021-04-17 14:48:54 +0300

    updated AddDataFragment.java and fragment_add_data.xml

--- app/src/main/java/com/example/harjoitustyo/AddDataFragment.java
+++ app/src/main/java/com/example/harjoitustyo/AddDataFragment.java
@@ -4,12 +4,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ScrollView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class AddDataFragment extends Fragment {
+public class AddDataFragment extends Fragment implements AdapterView.OnItemSelectedListener {
+    ScrollView scrollview;
+    TextView text;
+    Spinner spinner;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -19,5 +27,24 @@ public class AddDataFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        scrollview = (ScrollView) getView().findViewById(R.id.scrollView2);
+        text = (TextView) getView().findViewById(R.id.text);
+        spinner = (Spinner) getView().findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(), R.array.Choice, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+    }
+
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
