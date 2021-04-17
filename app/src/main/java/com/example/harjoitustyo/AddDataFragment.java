5e7c3f2d9b1f9f91ea226d60e664cbcaf67f1b1d
Joona <80720342+Jobe333@users.noreply.github.com>
2021-04-17 17:01:34 +0300

    updated AddDataFragment.java and fragment_add_data.xml

--- app/src/main/java/com/example/harjoitustyo/AddDataFragment.java
+++ app/src/main/java/com/example/harjoitustyo/AddDataFragment.java
@@ -16,7 +16,6 @@ import androidx.fragment.app.Fragment;
 
 public class AddDataFragment extends Fragment implements AdapterView.OnItemSelectedListener {
     ScrollView scrollview;
-    TextView text;
     Spinner spinner;
     @Nullable
     @Override
@@ -27,12 +26,68 @@ public class AddDataFragment extends Fragment implements AdapterView.OnItemSelec
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        scrollview = (ScrollView) getView().findViewById(R.id.scrollView2);
-        text = (TextView) getView().findViewById(R.id.text);
+
+        scrollview = (ScrollView) getView().findViewById(R.id.scrollView);
         spinner = (Spinner) getView().findViewById(R.id.spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(), R.array.Choice, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+        scrollview = (ScrollView) getView().findViewById(R.id.scrollView2);
+        spinner = (Spinner) getView().findViewById(R.id.spinner2);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(getActivity(), R.array.Choice, android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter2);
+        spinner.setOnItemSelectedListener(this);
+
+        scrollview = (ScrollView) getView().findViewById(R.id.scrollView3);
+        spinner = (Spinner) getView().findViewById(R.id.spinner3);
+        ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(getActivity(), R.array.Choice, android.R.layout.simple_spinner_item);
+        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter3);
+        spinner.setOnItemSelectedListener(this);
+
+        scrollview = (ScrollView) getView().findViewById(R.id.scrollView4);
+        spinner = (Spinner) getView().findViewById(R.id.spinner4);
+        ArrayAdapter<CharSequence> adapter4 = ArrayAdapter.createFromResource(getActivity(), R.array.Choice, android.R.layout.simple_spinner_item);
+        adapter4.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter4);
+        spinner.setOnItemSelectedListener(this);
+
+        scrollview = (ScrollView) getView().findViewById(R.id.scrollView5);
+        spinner = (Spinner) getView().findViewById(R.id.spinner5);
+        ArrayAdapter<CharSequence> adapter5 = ArrayAdapter.createFromResource(getActivity(), R.array.Choice, android.R.layout.simple_spinner_item);
+        adapter5.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter5);
+        spinner.setOnItemSelectedListener(this);
+
+        scrollview = (ScrollView) getView().findViewById(R.id.scrollView6);
+        spinner = (Spinner) getView().findViewById(R.id.spinner6);
+        ArrayAdapter<CharSequence> adapter6 = ArrayAdapter.createFromResource(getActivity(), R.array.Choice, android.R.layout.simple_spinner_item);
+        adapter6.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter6);
+        spinner.setOnItemSelectedListener(this);
+
+        scrollview = (ScrollView) getView().findViewById(R.id.scrollView7);
+        spinner = (Spinner) getView().findViewById(R.id.spinner7);
+        ArrayAdapter<CharSequence> adapter7 = ArrayAdapter.createFromResource(getActivity(), R.array.Choice, android.R.layout.simple_spinner_item);
+        adapter7.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter7);
+        spinner.setOnItemSelectedListener(this);
+
+        scrollview = (ScrollView) getView().findViewById(R.id.scrollView8);
+        spinner = (Spinner) getView().findViewById(R.id.spinner8);
+        ArrayAdapter<CharSequence> adapter8 = ArrayAdapter.createFromResource(getActivity(), R.array.Choice, android.R.layout.simple_spinner_item);
+        adapter8.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter8);
+        spinner.setOnItemSelectedListener(this);
+
+        scrollview = (ScrollView) getView().findViewById(R.id.scrollView9);
+        spinner = (Spinner) getView().findViewById(R.id.spinner9);
+        ArrayAdapter<CharSequence> adapter9 = ArrayAdapter.createFromResource(getActivity(), R.array.Choice_amount, android.R.layout.simple_spinner_item);
+        adapter9.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter9);
         spinner.setOnItemSelectedListener(this);
     }
 
