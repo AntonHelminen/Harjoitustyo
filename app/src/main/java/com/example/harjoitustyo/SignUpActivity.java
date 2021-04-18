9f453614fe348f1425df165a6992847b565ebd8c
Joona <80720342+Jobe333@users.noreply.github.com>
2021-04-18 16:19:09 +0300

    updated SignUpActivity.java and activity_sign_up.xml

--- app/src/main/java/com/example/harjoitustyo/SignUpActivity.java
+++ app/src/main/java/com/example/harjoitustyo/SignUpActivity.java
@@ -1,24 +1,37 @@
 package com.example.harjoitustyo;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class SignUpActivity extends AppCompatActivity {
+import com.google.android.material.textfield.TextInputLayout;
 
-    EditText editName, editAge, editUsername, editPassword, editConfirm;
+public class SignUpActivity extends AppCompatActivity {
+    private EditText editName, editAge, editUsername, editPassword, editConfirm;
+    String notification;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        editName = (EditText) findViewById(R.id.input_name);
-        editAge = (EditText) findViewById(R.id.input_age);
-        editUsername = (EditText) findViewById(R.id.input_username);
-        editPassword = (EditText) findViewById(R.id.input_password);
-        editConfirm = (EditText) findViewById(R.id.input_confirm);
+        editName = (EditText) findViewById(R.id.edit_name);
+        editAge = (EditText) findViewById(R.id.edit_age);
+        editUsername = (EditText) findViewById(R.id.edit_username);
+        editPassword = (EditText) findViewById(R.id.edit_password);
+        editConfirm = (EditText) findViewById(R.id.edit_confirm);
+
+    }
+
+    public void SignUp(View v)  {
+        notification = "Welcome!";
+        Toast.makeText(SignUpActivity.this, notification, Toast.LENGTH_SHORT).show();
+    }
+
+    public void GoBack(View v)  {
 
     }
 }
