7e7a3a796bdcf3466eb8b5e4887a5ec0b43bd22b
Joona <80720342+Jobe333@users.noreply.github.com>
2021-04-24 15:57:49 +0300

    updated fragment_profile.xml and ProfileFragment.java

--- app/src/main/java/com/example/harjoitustyo/ProfileFragment.java
+++ app/src/main/java/com/example/harjoitustyo/ProfileFragment.java
@@ -4,12 +4,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class ProfileFragment extends Fragment {
+    TextView textName;
+    TextView textAge;
+    CalendarView calender;
 
     @Nullable
     @Override
@@ -20,6 +25,22 @@ public class ProfileFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
+        textName = (TextView) getView().findViewById(R.id.textViewName);
+        textAge = (TextView) getView().findViewById(R.id.textViewAge);
+
+        User user = User.getInstance();
+
+        textName.setText(user.getPerson().getName());
+        textAge.setText(user.getPerson().getAge());
+
+        calender = (CalendarView) getView().findViewById(R.id.calendarView);
+        calender.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+
+            }
+        });
         super.onViewCreated(view, savedInstanceState);
     }
 }
